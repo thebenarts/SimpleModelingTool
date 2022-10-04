3af95883a1cf4e1dec8568929b1f74c9248eb881
MasterSlicer69 <hajlerzoltan@gmail.com>
2022-10-04 14:55:23 +0200

    test

--- src/Application.cpp
+++ src/Application.cpp
@@ -14,7 +14,7 @@
 #include <GLFW/glfw3.h>
 // RUN THIS ON X64
 // This is a test change
-
+//hello there
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
