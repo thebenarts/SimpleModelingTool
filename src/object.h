899996de7288dd0790302c4546cac789800bd3a8
szEdmond <edmondszakacs@gmail.com>
2022-10-06 16:14:20 +0200

    merge

--- src/object.h
+++ src/object.h
@@ -2,18 +2,21 @@
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
-
+#include "shader_m.h"
+// asd
 class Object 
 {
 	public:
-		Object();
-
-		
-		void Draw();
-	private:
 		unsigned int objectID;
 		bool bVisibility;
 		glm::vec3 location;
 		glm::vec3 rotation;
+
+		Object();
+
+		virtual void Draw(Shader* shader);
+	private:
+		
+		
 
 };
